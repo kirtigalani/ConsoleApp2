d862f8ccacd796e8943a01a41cdd7618d132c04e
kirtigalani <kittugalani@gmail.com>
2024-11-28 22:43:57 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 0;
+            //Console.WriteLine("Min value:" + int.MinValue);
+            //Console.WriteLine("Max value:" + int.MaxValue);
+
+            //string s="\"Hello\"";
+            //string s = "One\ntwo\nthree";
+            //string s = "\\One\\two\\three\\";
+            string s = @"\\One\\two\\three\\";
+            Console.WriteLine(s);
+
+        }
+    }
+}
